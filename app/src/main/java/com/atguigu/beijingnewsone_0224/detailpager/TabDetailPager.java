4e2b6b68aa9d3dd11314b86516364bd68941e78e
tianxuewei1016 <93226539@qq.com>
2017-06-05 13:06:28 +0800

    配置联网并且请求&解析数据

--- app/src/main/java/com/atguigu/beijingnewsone_0224/detailpager/TabDetailPager.java
+++ app/src/main/java/com/atguigu/beijingnewsone_0224/detailpager/TabDetailPager.java
@@ -10,9 +10,15 @@ import android.widget.TextView;
 import com.atguigu.beijingnewsone_0224.R;
 import com.atguigu.beijingnewsone_0224.base.MenuDetailBasePager;
 import com.atguigu.beijingnewsone_0224.domain.NewsCenterBean;
+import com.atguigu.beijingnewsone_0224.domain.TabDetailPagerBean;
+import com.atguigu.beijingnewsone_0224.utils.Constants;
+import com.google.gson.Gson;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import okhttp3.Call;
 
 /**
  * 作者：田学伟 on 2017/6/5 09:53
@@ -41,13 +47,39 @@ public class TabDetailPager extends MenuDetailBasePager {
     public View initView() {
         //创建子类的视图
         View view = View.inflate(mContext, R.layout.pager_tab_detail, null);
-        ButterKnife.inject(this,view);
+        ButterKnife.inject(this, view);
         return view;
     }
 
     @Override
     public void initData() {
         super.initData();
+        getDataFromNet();
+    }
 
+    /**
+     * 网络请求
+     */
+    private void getDataFromNet() {
+        OkHttpUtils.get()
+                .url(Constants.BASE_URL + childrenBean.getUrl())
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+//                        Log.e("TAG", "请求失败==" + e.getMessage());
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+//                        Log.e("TAG", "请求成功==" + response);
+                        processData(response);
+                    }
+                });
+    }
+
+    private void processData(String response) {
+        TabDetailPagerBean bean = new Gson().fromJson(response, TabDetailPagerBean.class);
+//        Log.e("TAG", "" + bean.getData().getNews().get(0).getTitle());
     }
 }
